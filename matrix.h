0d40ae6329a8c20055cede300d6b62ca69677710
Ivan <ivanbasurman88@yandex.ru>
2018-03-19 10:36:02 +0300

    delete class Element

--- matrix.h
+++ matrix.h
@@ -2,40 +2,38 @@
 #include <map>
 #include <algorithm>
 #include <tuple>
+#include <memory>
 #include "element.h"
 #include "proxy.h"
-
-template<class T>
-class RealElement;
 
 
 template<class T, T N>
 class Matrix{
-
-    using IteratorType = typename std::map<std::pair<long, long>, RealElement<T>* >::iterator;
+    
+    using IteratorType = typename std::map<std::pair<long, long>, T>::iterator;
+    
 public:
-    Matrix() : defaultElement(new RealElement<T>(N))
+    Matrix() : defaultElement(N)
     {};
-
+    
     ~Matrix(){
-        delete defaultElement;
     }
-
+    
     Proxy<T, N> operator[](long x){
         Proxy<T, N> proxy(this, x);
         return proxy;
     };
-
+    
     size_t size(){
         return matrix.size();
     }
-
-    RealElement<T>* getDefaultElement(){
+    
+    T getDefaultElement(){
         return defaultElement;
     };
-
+    
     //поиск элемента
-    RealElement<T>* findByCoord(std::pair<long, long> coordinates){
+    const T& findByCoord(std::pair<long, long> coordinates) const {
         auto it = matrix.find(coordinates);
         if( it == matrix.end()){
             return defaultElement;
@@ -44,60 +42,60 @@ public:
             return it->second;
         }
     };
-
+    
     //удаление элемента
     void removeElement(std::pair<long, long> coordinates){
         matrix.erase(matrix.find(coordinates));
     };
-
+    
     //вставка элемента
     void insertElement(std::pair<long, long> coordinates, T value){
-        matrix.emplace(coordinates, new RealElement<T>(value));
+        matrix.emplace(coordinates, value);
     };
-
-
+    
     //итератор
     class Iterator{
     public:
         Iterator(IteratorType  matrixIt_):iterator(matrixIt_){};
-
+        
         IteratorType getInsideIterator(){
             return this->iterator;
         }
-
+        
         void operator++(){
             iterator++;
         };
-
+        
         std::tuple<long, long, T> operator*(){
-            return std::make_tuple(iterator->first.first, iterator->first.second, iterator->second->getValue());
+            return std::make_tuple(iterator->first.first, iterator->first.second, iterator->second);
         };
-
+        
         void operator--(){
             iterator--;
         }
-
+        
         bool operator !=(Iterator& other){
             return iterator != other.getInsideIterator();
         }
-
+        
         bool operator ==(Iterator& other){
             return iterator == other.getInsideIterator();
         }
-
+        
     private:
         IteratorType  iterator;
     };
-
-
+    
+    
     Iterator begin(){
         return Iterator(matrix.begin());
     };
-
+    
     Iterator end(){
         return Iterator(matrix.end());
     };
 private:
-  RealElement<T> * defaultElement;
-  std::map<std::pair<long, long>, RealElement<T>* > matrix;
+    
+    const T defaultElement;
+    std::map<std::pair<long, long>, T > matrix;
 };
--- proxy.h
+++ proxy.h
@@ -2,30 +2,32 @@
 
 #include "matrix.h"
 #include "element.h"
+#include <memory>
 
 template<class T, T N>
 class Matrix;
 
-template<class T>
-class RealElement;
 
 template<class T, T N>
 class Proxy{
 public:
     Proxy(Matrix<T, N> * matrixPtr_, long x_) :
-        matrixPtr(matrixPtr_), element(nullptr), x(x_)
+    matrixPtr(matrixPtr_), element(0), x(x_)
     {};
-
+    
+    ~Proxy(){
+    };
+    
     Proxy& operator[](int y){
         coord = std::make_pair(x, y);
         element = matrixPtr->findByCoord(coord);
         return *this;
     };
-
+    
     void operator=(T newValue){
-        if(newValue != matrixPtr->getDefaultElement()->getValue()){
+        if(newValue != matrixPtr->getDefaultElement()){
             if(element != matrixPtr->getDefaultElement()){
-                element->setValue(newValue);
+                element = newValue;
             }
             else{
                 matrixPtr->insertElement(coord, newValue);
@@ -39,14 +41,14 @@ public:
             else return;
         }
     };
-
+    
     operator T(){
-        return element->getValue();
+        return element;
     }
-
+    
 private:
     long x;
     std::pair<long, long> coord;
-    RealElement<T> * element;
+    T element;
     Matrix<T, N>*  matrixPtr;
 };
